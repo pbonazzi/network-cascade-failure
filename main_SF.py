519981c50c67e535de42a44dc186de7bbc32badd
hk-kaden-kim <hyeongkyun.kim@uzh.ch>
2021-12-10 23:40:06 +0100

    prepare code for SF figure

--- main_SF.py
+++ main_SF.py
@@ -3,21 +3,21 @@ from src.create import *
 from src.measure import *
 
 print("\n++++++++++++++++++++++++++++++")
-print("SF N2000 Test Start...")
+print("SF N2000 gamma 2.5 Test Start...")
 print("++++++++++++++++++++++++++++++\n")
 
 SFn2000_25 = generate_pinf_SF(2000, 2.5, 10)
 np.savetxt('./notebooks/results/1012/SFn2000_25.csv', SFn2000_25, delimiter=',')
 
 print("\n++++++++++++++++++++++++++++++")
-print("SF N2000 Test Start...")
+print("SF N2000 gamma 2.7 Test Start...")
 print("++++++++++++++++++++++++++++++\n")
 
 SFn2000_27 = generate_pinf_SF(2000, 2.7, 10)
 np.savetxt('./notebooks/results/1012/SFn2000_27.csv', SFn2000_27, delimiter=',')
 
 print("\n++++++++++++++++++++++++++++++")
-print("SF N2000 Test Start...")
+print("SF N2000 gamma 3.0 Test Start...")
 print("++++++++++++++++++++++++++++++\n")
 
 SFn2000_30 = generate_pinf_SF(2000, 3, 10)
