218fc20557b0524d2a5b5c9f78c3d18f490a00a1
hk-kaden-kim <hyeongkyun.kim@uzh.ch>
2021-12-13 16:47:15 +0100

    Add avg.degree feature to SF Gen

--- main_SF.py
+++ main_SF.py
@@ -23,22 +23,21 @@ from datetime import datetime
 # SFn2000_30 = generate_pinf_SF(t=10, hasGraph=True, files = gPaths)
 # np.savetxt('./notebooks/results/1012/SFn2000_30.csv', SFn2000_30, delimiter=',')
 
-
 print("\n++++++++++++++++++++++++++++++")
-print("SF N50000 gamma 2.3 <k> = 4 Graph Generte...")
+print("SF N50000 gamma 3.0 <k> = 4 Graph Generte...")
 print("++++++++++++++++++++++++++++++\n")
 
 start = datetime.now()
-SFn50000_23_a = networkSF_w_3Dpos_PowerL(50000,2.3,4,1)
-nx.write_gpickle(SFn50000_23_a, "../data/pickle/SF/SFn50000_23_a.gpickle")
+SFn50000_30_a = networkSF_w_3Dpos_PowerL(50000,3.0,4,1)
+nx.write_gpickle(SFn50000_30_a, "../data/pickle/SF/SFn50000_30_a.gpickle")
 time = datetime.now() - start
-print("...SFn50000_23_a Graph Generate Done!", time)
+print("...SFn50000_30_a Graph Generate Done!", time)
 
 start = datetime.now()
-SFn50000_23_b = networkSF_w_3Dpos_PowerL(50000,2.3,4,2)
-nx.write_gpickle(SFn50000_23_b, "../data/pickle/SF/SFn50000_23_b.gpickle")
+SFn50000_30_b = networkSF_w_3Dpos_PowerL(50000,3.0,4,2)
+nx.write_gpickle(SFn50000_30_b, "../data/pickle/SF/SFn50000_30_b.gpickle")
 time = datetime.now() - start
-print("...SFn50000_23_b Graph Generate Done!", time)
+print("...SFn50000_30_b Graph Generate Done!", time)
 
 print("\n++++++++++++++++++++++++++++++")
 print("SF N50000 gamma 2.7 <k> = 4 Graph Generte...")
@@ -56,22 +55,22 @@ nx.write_gpickle(SFn50000_27_b, "../data/pickle/SF/SFn50000_27_b.gpickle")
 time = datetime.now() - start
 print("...SFn50000_27_b Graph Generate Done!", time)
 
+
 print("\n++++++++++++++++++++++++++++++")
-print("SF N50000 gamma 3.0 <k> = 4 Graph Generte...")
+print("SF N50000 gamma 2.3 <k> = 4 Graph Generte...")
 print("++++++++++++++++++++++++++++++\n")
 
 start = datetime.now()
-SFn50000_30_a = networkSF_w_3Dpos_PowerL(50000,3.0,4,1)
-nx.write_gpickle(SFn50000_30_a, "../data/pickle/SF/SFn50000_30_a.gpickle")
+SFn50000_23_a = networkSF_w_3Dpos_PowerL(50000,2.3,4,1)
+nx.write_gpickle(SFn50000_23_a, "../data/pickle/SF/SFn50000_23_a.gpickle")
 time = datetime.now() - start
-print("...SFn50000_30_a Graph Generate Done!", time)
+print("...SFn50000_23_a Graph Generate Done!", time)
 
 start = datetime.now()
-SFn50000_30_b = networkSF_w_3Dpos_PowerL(50000,3.0,4,2)
-nx.write_gpickle(SFn50000_30_b, "../data/pickle/SF/SFn50000_30_b.gpickle")
+SFn50000_23_b = networkSF_w_3Dpos_PowerL(50000,2.3,4,2)
+nx.write_gpickle(SFn50000_23_b, "../data/pickle/SF/SFn50000_23_b.gpickle")
 time = datetime.now() - start
-print("...SFn50000_30_b Graph Generate Done!", time)
-
+print("...SFn50000_23_b Graph Generate Done!", time)
 
 # print("\n++++++++++++++++++++++++++++++")
 # print("SF N2000 gamma 2.5 Test Start...")
