77434f85dcf5370eb06b4fd567637437114dac56
hk-kaden-kim <hyeongkyun.kim@uzh.ch>
2021-12-15 10:06:27 +0100

    Add avg.degree feature to SF Gen

--- main_SF.py
+++ main_SF.py
@@ -13,41 +13,41 @@ import click
 
 
 def main(n, g, k):
-    file_a = "data/pickle/SF/SFn"+str(n)+"_"+str(int(g*10))+"_k"+str(int(k))+"_a.gpickle"
-    file_b = "data/pickle/SF/SFn"+str(n)+"_"+str(int(g*10))+"_k"+str(int(k))+"_b.gpickle"
-    file_intd = "data/pickle/SF/int_SFn"+str(n)+"_"+str(int(g*10))+"_k"+str(int(k))+".gpickle"
-
-    print("\n++++++++++++++++++++++++++++++")
-    print("v1.0 SF N%d gamma %f <k> = %f Graph Cascade Test..." %(n,g,k))
-    print("++++++++++++++++++++++++++++++\n")
-
-    gPaths = [file_a,file_b,file_intd]
-    result = generate_pinf_SF(t=50, hasGraph=True, files = gPaths)
-    filepath = "notebooks/results/1012/SFn"+str(n)+"_"+str(int(g*10))+"_k"+str(int(k))+".csv"
-    np.savetxt(filepath, result, delimiter=',')
+    # file_a = "data/pickle/SF/SFn"+str(n)+"_"+str(int(g*10))+"_k"+str(int(k))+"_a.gpickle"
+    # file_b = "data/pickle/SF/SFn"+str(n)+"_"+str(int(g*10))+"_k"+str(int(k))+"_b.gpickle"
+    # file_intd = "data/pickle/SF/int_SFn"+str(n)+"_"+str(int(g*10))+"_k"+str(int(k))+".gpickle"
 
     # print("\n++++++++++++++++++++++++++++++")
-    # print("v1.0 SF N%d gamma %f <k> = %f Graph Generate..." %(n,g,k))
+    # print("v1.0 SF N%d gamma %f <k> = %f Graph Cascade Test..." %(n,g,k))
     # print("++++++++++++++++++++++++++++++\n")
 
-    # SFn_a = networkSF_w_3Dpos_PowerL(n,g,k,1)
-    # filepath = "data/pickle/SF/SFn"+str(n)+"_"+str(int(g*10))+"_k"+str(int(k))+"_a.gpickle"
-    # nx.write_gpickle(SFn_a, filepath)
-    # print("SF N%d gamma %f <k> = %f A Graph Done..." %(n,g,k))
-    # print(filepath+"\n")
+    # gPaths = [file_a,file_b,file_intd]
+    # result = generate_pinf_SF(t=50, hasGraph=True, files = gPaths)
+    # filepath = "notebooks/results/1012/SFn"+str(n)+"_"+str(int(g*10))+"_k"+str(int(k))+".csv"
+    # np.savetxt(filepath, result, delimiter=',')
 
-    # SFn_b = networkSF_w_3Dpos_PowerL(n,g,k,2)
-    # filepath = "data/pickle/SF/SFn"+str(n)+"_"+str(int(g*10))+"_k"+str(int(k))+"_b.gpickle"
-    # nx.write_gpickle(SFn_b, filepath)
-    # print("SF N%d gamma %f <k> = %f B Graph Done..." %(n,g,k))
-    # print(filepath+"\n")
+    print("\n++++++++++++++++++++++++++++++")
+    print("v1.0 SF N%d gamma %f <k> = %f Graph Generate..." %(n,g,k))
+    print("++++++++++++++++++++++++++++++\n")
+
+    SFn_a = networkSF_w_3Dpos_PowerL(n,g,k,1)
+    filepath = "data/pickle/SF/SFn"+str(n)+"_"+str(int(g*10))+"_k"+str(int(k))+"_a.gpickle"
+    nx.write_gpickle(SFn_a, filepath)
+    print("SF N%d gamma %f <k> = %f A Graph Done..." %(n,g,k))
+    print(filepath+"\n")
+
+    SFn_b = networkSF_w_3Dpos_PowerL(n,g,k,2)
+    filepath = "data/pickle/SF/SFn"+str(n)+"_"+str(int(g*10))+"_k"+str(int(k))+"_b.gpickle"
+    nx.write_gpickle(SFn_b, filepath)
+    print("SF N%d gamma %f <k> = %f B Graph Done..." %(n,g,k))
+    print(filepath+"\n")
 
 
-    # intd_SF = intd_random_net(SFn_a,SFn_b)
-    # filepath = "data/pickle/SF/int_SFn"+str(n)+"_"+str(int(g*10))+"_k"+str(int(k))+".gpickle"
-    # nx.write_gpickle(intd_SF, filepath)
-    # print("intd SF N%d gamma %f <k> = %f B Graph Done..." %(n,g,k))
-    # print(filepath+"\n")
+    intd_SF = intd_random_net(SFn_a,SFn_b)
+    filepath = "data/pickle/SF/int_SFn"+str(n)+"_"+str(int(g*10))+"_k"+str(int(k))+".gpickle"
+    nx.write_gpickle(intd_SF, filepath)
+    print("intd SF N%d gamma %f <k> = %f B Graph Done..." %(n,g,k))
+    print(filepath+"\n")
 
 
 
