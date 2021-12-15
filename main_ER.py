293aa2fe3bbbc571f851d7a4d55b30cb34cfb26d
pbonazzi <pietro.bonazzi@uzh.ch>
2021-12-15 23:19:56 +0000

    main_ER

--- main_ER.py
+++ main_ER.py
@@ -6,22 +6,22 @@ print("\n++++++++++++++++++++++++++++++")
 print("ER N1000 Test Start...")
 print("++++++++++++++++++++++++++++++\n")
 
-ERn1000k4 = generate_pinf_ER(1000, 4, 10)
-np.savetxt('./notebooks/results/1012/ERn1000k4.csv', ERn1000k4, delimiter=',')
+#ERn1000k4 = generate_pinf_ER(1000, 4, 10)
+#np.savetxt('./notebooks/results/1012/ERn1000k4.csv', ERn1000k4, delimiter=',')
 
 print("\n++++++++++++++++++++++++++++++")
 print("ER N2000 Test Start...")
 print("++++++++++++++++++++++++++++++\n")
 
-ERn2000k4 = generate_pinf_ER(2000, 4, 10)
-np.savetxt('./notebooks/results/1012/ERn2000k4.csv', ERn2000k4, delimiter=',')
+#ERn2000k4 = generate_pinf_ER(2000, 4, 10)
+#np.savetxt('./notebooks/results/1012/ERn2000k4.csv', ERn2000k4, delimiter=',')
 
 print("\n++++++++++++++++++++++++++++++")
 print("ER N4000 Test Start...")
 print("++++++++++++++++++++++++++++++\n")
 
-ERn4000k4 = generate_pinf_ER(4000, 4, 10)
-np.savetxt('./notebooks/results/1012/ERn4000k4.csv', ERn4000k4, delimiter=',')
+#ERn4000k4 = generate_pinf_ER(4000, 4, 10)
+#np.savetxt('./notebooks/results/1012/ERn4000k4.csv', ERn4000k4, delimiter=',')
 
 print("\n++++++++++++++++++++++++++++++")
 print("ER N8000 Test Start...")
