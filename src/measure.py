5bb6b1a9edb2f08d69c7b41615cd54e745ec2e75
hk-kaden-kim <hyeongkyun.kim@uzh.ch>
2021-12-15 16:33:27 +0100

    SF Figure Update

--- src/measure.py
+++ src/measure.py
@@ -178,11 +178,12 @@ def plot_pinf(results, k=1, xlim=None, labels=None, path=None, p_theory=False):
     """
     plt.rcParams.update({'font.size': 14})
     color = iter(plt.cm.rainbow(np.linspace(0.0, 1, len(results))))
+    marker = ['v', 's', 'D','v']
 
     for i, res in enumerate(results):
         pks = res[0]*k
         p_infs = res[1]
-        plt.plot(pks, p_infs, c=next(color), label=labels[i])
+        plt.plot(pks, p_infs, c=next(color), label=labels[i], marker=marker[i], mfc = 'none')
     if p_theory :
         plt.vlines(2.4554, ymin=0, ymax=1, colors='k', linestyles='solid', label='$p_{c}$=2.4554/<k>')
     if k > 1:
